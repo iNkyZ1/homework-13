f92af45606311ea7110b258f8feed79967c41a13
Nick <n.t.1996@mail.ru>
2025-12-05 23:29:53 +0400

    feat: implement Signin form with onSubmit

---
+++ src/components/Signin/Signin.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import TextInput from "../TextInput/TextInput";
+
+function Signin(props) {
+  const { onSubmit } = props;
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    const formData = {
+      email: email,
+      password: password,
+    };
+
+    if (typeof onSubmit === "function") {
+      onSubmit(formData);
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Sign in</h2>
+
+      <TextInput
+        label="Email"
+        type="email"
+        name="email"
+        placeholder="you@example.com"
+        required={true}
+        value={email}
+        onChange={function (event) {
+          setEmail(event.target.value);
+        }}
+      />
+
+      <TextInput
+        label="Password"
+        type="password"
+        name="password"
+        placeholder="Your password"
+        required={true}
+        value={password}
+        onChange={function (event) {
+          setPassword(event.target.value);
+        }}
+      />
+
+      <button type="submit">Войти</button>
+    </form>
+  );
+}
+
+export default Signin;
