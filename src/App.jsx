3fc4c55ec6c6922cdf7ee3d7c3b003fe74126928
Nick <n.t.1996@mail.ru>
2025-12-05 23:31:25 +0400

    feat: connect Signin and Signup forms in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
 import React from "react";
+import Signin from "./components/Signin/Signin";
+import Signup from "./components/Signup/Signup";
 
 function App() {
+  function handleSigninSubmit(values) {
+    console.log("Signin form values:", values);
+  }
+
+  function handleSignupSubmit(values) {
+    console.log("Signup form values:", values);
+  }
+
   return (
     <div style={{ maxWidth: 480, margin: "0 auto", padding: "24px" }}>
-      <h1>Forms</h1>
-      <p>Подключим Signin и Signup.</p>
+      <h1>Forms demo</h1>
+
+      <div
+        style={{
+          marginBottom: "32px",
+          borderBottom: "1px solid #eee",
+          paddingBottom: "24px",
+        }}
+      >
+        <Signin onSubmit={handleSigninSubmit} />
+      </div>
+
+      <div>
+        <Signup onSubmit={handleSignupSubmit} />
+      </div>
     </div>
   );
 }
